20440dbd6855464795784b5fabbfbbf2bc307a58
AleisterFox <ericaguirrez@hotmail.com>
2024-04-29 10:19:53 -0600

    pagina de personajes

---
+++ personajes.php
@@ -0,0 +1,174 @@
+<!DOCTYPE html>
+<html lang="es" xml:lang="es" style="overflow-x: visible">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="author" content="Molcajete" />
+    <meta name="description" content="¿Buscas hacer de la fiesta de tu hijo un evento inolvidable? En [Nombre del Dilo Con Botargass la mejor selección de botargas para fiestas infantiles. Encuentra a tus personajes favoritos y haz que la celebración sea aún más especial. ¡Contáctanos ahora para reservar la diversión!" />
+
+    <meta name="Keywords" content="renta de botargas, botargas para fiestas infantiles, personajes para fiestas infantiles, botargas divertidas, fiestas infantiles temáticas, animación para fiestas de niños, alquiler de disfraces para fiestas, renta de disfraces para eventos infantiles, personajes de caricaturas para fiestas, animadores para fiestas de niños" />
+
+    <meta name="robots" content="all" />
+    <meta property="og:title" content="Dilo Con Botargas" />
+    <meta property="og:description" content="¿Buscas hacer de la fiesta de tu hijo un evento inolvidable? En Dilo Con Botargas te ofrecemos la mejor selección de botargas para fiestas infantiles. Encuentra a tus personajes favoritos y haz que la celebración sea aún más especial. ¡Contáctanos ahora para reservar la diversión!" />
+    <meta property="og:type" content="website" />
+    <meta property="og:url" content="" />
+    <meta property="og:image" content="../img/imagen.png" />
+    <meta property="og:image:width" content="300" />
+    <meta property="og:image:height" content="300" />
+    <meta property="og:site_name" content="Dilo Con Botargas" />
+    <meta name="msapplication-TileColor" content="rgb(16, 48, 55)" />
+    <meta name="theme-color" content="rgb(16, 48, 55)" />
+
+    <link href="https://cdn.jsdelivr.net/gh/eliyantosarage/font-awesome-pro@main/fontawesome-pro-6.5.1-web/css/all.min.css" rel="stylesheet" />
+    <link rel="stylesheet" href="css/style.css" />
+    <link rel="stylesheet" href="css/animate.css" />
+    <link rel="shortcut icon" href="img/icon.png" type="image/x-icon" media="(prefers-color-scheme: light)" />
+    <link rel="shortcut icon" href="img/icon.png" type="image/x-icon" media="(prefers-color-scheme: dark)" />
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link href="https://fonts.googleapis.com/css?family=Poppins:100,100italic,200,200italic,300,300italic,regular,italic,500,500italic,600,600italic,700,700italic,800,800italic,900,900italic" rel="stylesheet" />
+    <link rel="stylesheet" href="css/loader.css" />
+
+    <script src="js/wow.min.js"></script>
+    <script>
+        new WOW().init();
+    </script>
+    <title>Dilo Con Botargas</title>
+</head>
+
+<body>
+    <div class="whatsapp">
+        <a href="https://wa.link/d53e1z" target="_blank"><i class="fab fa-whatsapp"></i></a>
+    </div>
+
+    <!-- <div class="loader__screen" id="loader">
+    <div id="wifi-loader">
+      <svg class="circle-outer" viewBox="0 0 86 86">
+        <circle class="back" cx="43" cy="43" r="40"></circle>
+        <circle class="front" cx="43" cy="43" r="40"></circle>
+        <circle class="new" cx="43" cy="43" r="40"></circle>
+      </svg>
+      <svg class="circle-middle" viewBox="0 0 60 60">
+        <circle class="back" cx="30" cy="30" r="27"></circle>
+        <circle class="front" cx="30" cy="30" r="27"></circle>
+      </svg>
+      <svg class="circle-inner" viewBox="0 0 34 34">
+        <circle class="back" cx="17" cy="17" r="14"></circle>
+        <circle class="front" cx="17" cy="17" r="14"></circle>
+      </svg>
+      <div class="text" data-text="Loading..."></div>
+    </div>
+  </div> -->
+
+    <?php include 'elements/header.php' ?>
+
+    <main>
+
+        <section id="contenido" class="personajes">
+            <h2>Personajes</h2>
+            <select name="personajes" id="personajes">
+                    <option value=NULL>Personajes</option>
+                </select>
+            <div class="container renta">
+                <div class="top">
+                    <div class="big">
+                        <figure>
+                            <img src="img/mickey.jpg" alt="">
+                        </figure>
+                    </div>
+                    <div class="small">
+                        <figure><img src="img/mickey.jpg" alt=""></figure>
+                        <figure><img src="img/mickey.jpg" alt=""></figure>
+                    </div>
+                </div>
+                <div class="bottom">
+                    <div class="small">
+                        <figure><img src="img/mickey.jpg" alt=""></figure>
+                        <figure><img src="img/mickey.jpg" alt=""></figure>
+                    </div>
+                    <div class="big">
+                        <figure>
+                            <img src="img/mickey.jpg" alt="">
+                        </figure>
+                    </div>
+                </div>
+            </div>
+
+        </section>
+
+    </main>
+
+    <?php include 'elements/footer.php' ?>
+
+</body>
+<script src="js/script.js"></script>
+
+<script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
+<script src="js/loader.js"></script>
+
+<script>
+  const personajes = [
+    "Mickey",
+    "Minnie",
+    "Doctor Simi",
+    "Mario Bross",
+    "Luigui",
+    "Bowser",
+    "Princesa Peach",
+    "Bestia",
+    "Stitch",
+    "Gallo Bartolito",
+    "Vaca Lola",
+    "kitty",
+    "Kuromy LOADING",
+    "Unicornio",
+    "Messi",
+    "Karol G",
+    "BadBunny",
+    "Carin Leon LOADING",
+    "Luis Miguel",
+    "PesoPluma",
+    "La Mascara LOADING",
+    "JaimeDuende",
+    "Daddy Yankee",
+    "Jack Skellington",
+    "Beetlejuice LOADING",
+    "Pikachu",
+    "Bebes LOADING",
+    "Sonic",
+    "Oso",
+    "Oso de Masha",
+    "Chase",
+    "Barbie",
+    "Bob Sponja",
+    "Spiderman",
+    "PlimPlim",
+    "Bluey",
+    "Blue",
+    "Beto",
+    "Minions",
+    "Sulley Monsters Inc",
+    "Peppa Pig",
+    "Corazón",
+    "Dora la exploradora",
+    "Alex Leon de Madagascar",
+    "Scooby Doo",
+    "Grinch",
+    "Santa Claus",
+    "Reno",
+    "Olaf"
+  ];
+
+  const select = document.getElementById('personajes');
+
+  personajes.forEach((personaje, index) => {
+    const option = document.createElement("option");
+    option.text = personaje;
+    option.value = index + 1;
+    select.add(option);
+  });
+</script>
+
+</html>
