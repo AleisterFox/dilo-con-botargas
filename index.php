1378004d1e197ecb62cbfcf60c6a1ac4454b2c23
AleisterFox <ericaguirrez@hotmail.com>
2024-05-06 11:06:38 -0600

    cambiar cdn iconos

--- index.php
+++ index.php
@@ -21,17 +21,7 @@
   <meta name="msapplication-TileColor" content="rgb(16, 48, 55)" />
   <meta name="theme-color" content="rgb(16, 48, 55)" />
 
-  <link href="https://cdn.jsdelivr.net/gh/eliyantosarage/font-awesome-pro@main/fontawesome-pro-6.5.1-web/css/all.min.css" rel="stylesheet" />
-  <link rel="stylesheet" href="css/style.css" />
-  <link rel="stylesheet" href="css/animate.css" />
-  <link rel="shortcut icon" href="img/icon.png" type="image/x-icon" media="(prefers-color-scheme: light)" />
-  <link rel="shortcut icon" href="img/icon.png" type="image/x-icon" media="(prefers-color-scheme: dark)" />
-  <link rel="preconnect" href="https://fonts.googleapis.com" />
-  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-  <link href="https://fonts.googleapis.com/css?family=Poppins:100,100italic,200,200italic,300,300italic,regular,italic,500,500italic,600,600italic,700,700italic,800,800italic,900,900italic" rel="stylesheet" />
-  <link rel="stylesheet" href="node_modules/@glidejs/glide/dist/css/glide.core.min.css" />
-  <link rel="stylesheet" href="node_modules/@glidejs/glide/dist/css/glide.theme.min.css" />
-  <link rel="stylesheet" href="css/loader.css" />
+  <?php include 'elements/estilos.php'?>
 
   <script src="js/wow.min.js"></script>
   <script>
